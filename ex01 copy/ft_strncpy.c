758f5674cc9cf90812f67d1e4933638d78b22870
BryanLee92 <lwm.bryan92@gmail.com>
2022-03-05 18:04:34 +0800

    C02

---
+++ ex01 copy/ft_strncpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brlee <brlee@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 12:49:19 by brlee             #+#    #+#             */
+/*   Updated: 2022/02/23 14:31:03 by brlee            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i++] = '\0';
+	}
+	return (dest);
+}
